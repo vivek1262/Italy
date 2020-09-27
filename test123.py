6a2441388e5e149e8be429c8f510064a29eb394b
vivek <vivek254258@gmail.com>
2020-09-27 12:17:11 +0530

    new tkinter file

---
+++ test123.py
@@ -0,0 +1,22 @@
+str = "asdsfgfhhqwertyu"
+
+
+print(str[1:-6])
+
+
+
+list = [10,20,300,20,3,0,5,6,7]
+
+print(list[2:-2])
+
+
+tuple = (1,'sai',2)
+
+print(tuple[:-1])
+
+print("*****************************")
+
+import datetime
+
+date_col = datetime.date.today()
+print(date_col)
